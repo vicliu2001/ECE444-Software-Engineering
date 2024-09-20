808c6a40b008d7794b5aa8273e6f15fecc58e114
vicliu2001 <116909592+vicliu2001@users.noreply.github.com>
2024-09-19 21:06:53 -0400

    added script to sort projects by date

--- load_projects.js
+++ load_projects.js
@@ -10,6 +10,7 @@ const projects = [
       'Display AI generated comments and fashion advice',
     ],
     link: 'https://github.com/natapokie/ai-fashion-mirror',
+    time: '2024-09',
   },
   {
     title: 'Open Source Map',
@@ -22,56 +23,65 @@ const projects = [
       'Use GTK to design UI supporting multiple functions for map usage',
     ],
     link: 'https://docs.google.com/presentation/d/1sv3nVnLqu5-BgBSRExNNuLlefhMb_VeT/edit?usp=sharing&ouid=110080259429906623471&rtpof=true&sd=true',
+    time: '2022-04',
   },
-  // Add more projects as needed...
 ];
 
 let allProjectsVisible = false;
 
-// Function to display the latest project initially
-function displayLatestProject() {
-  const projectDiv = document.getElementById('recent-projects');
-  projectDiv.innerHTML = ''; // Clear existing content
+function findMostRecentProject(projects) {
+  projects.sort((a, b) => new Date(b.time) - new Date(a.time));
 
-  // Add first project (AI Fashion Mirror already in HTML)
-  let firstProjectHTML = `
-      <div class="col s12 m6 l4 project">
-        <div class="card medium">
-          <div class="card-image waves-effect waves-block waves-light">
-            <img alt="${projects[0].title}" src="${
-    projects[0].image
+  return projects[0];
+}
+
+function displayMostRecentProject() {
+  const recentProject = findMostRecentProject(projects);
+  const projectDiv = document.getElementById('recent-projects');
+
+  // Clear any existing content in the recent projects section
+  projectDiv.innerHTML = '';
+
+  // Add the most recent project to the section
+  let recentProjectHTML = `
+    <div class="col s12 m6 l4 project">
+      <div class="card medium">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img alt="${recentProject.title}" src="${
+    recentProject.image
   }" style="height: 100%; width: 100%" class="activator" />
-          </div>
-          <div class="card-content">
-            <span class="card-title activator teal-text hoverline">${
-              projects[0].title
-            }<i class="mdi-navigation-more-vert right"></i></span>
-            <p>${projects[0].description}</p>
-          </div>
-          <div class="card-reveal">
-            <span class="card-title grey-text"><small>Accomplishments</small><i class="mdi-navigation-close right"></i></span>
-            <ul>
-              ${projects[0].accomplishments
-                .map((accomplishment) => `<li>${accomplishment}</li>`)
-                .join('')}
-            </ul>
-            <div class="card-action">
-              <a href="${
-                projects[0].link
-              }" target="_blank" class="btn-floating btn-large waves-effect waves-light blue-grey tooltipped"><i class="fa fa-github"></i></a>
-            </div>
+        </div>
+        <div class="card-content">
+          <span class="card-title activator teal-text hoverline">${
+            recentProject.title
+          }<i class="mdi-navigation-more-vert right"></i></span>
+          <p>${recentProject.description}</p>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text"><small>Accomplishments</small><i class="mdi-navigation-close right"></i></span>
+          <ul>
+            ${recentProject.accomplishments
+              .map((accomplishment) => `<li>${accomplishment}</li>`)
+              .join('')}
+          </ul>
+          <div class="card-action">
+            <a href="${
+              recentProject.link
+            }" target="_blank" class="btn-floating btn-large waves-effect waves-light blue-grey tooltipped"><i class="fa fa-external-link"></i></a>
           </div>
         </div>
       </div>
-    `;
-  projectDiv.innerHTML = firstProjectHTML;
+    </div>
+  `;
+  projectDiv.innerHTML = recentProjectHTML;
 }
 
-// Function to display all remaining projects
+window.onload = displayMostRecentProject;
+
 function loadMoreProjects() {
   const projectDiv = document.getElementById('recent-projects');
 
-  if (!allProjectsVisible) {
+  if (allProjectsVisible == false) {
     let allProjectsContent = '';
 
     for (let i = 1; i < projects.length; i++) {
@@ -112,14 +122,10 @@ function loadMoreProjects() {
     document.getElementById('load-more-btn').textContent = 'Show Most Recent';
     allProjectsVisible = true;
   } else {
-    displayLatestProject(); // Reset to show the first project
+    displayMostRecentProject();
     document.getElementById('load-more-btn').textContent = 'Load More';
     allProjectsVisible = false;
   }
 }
 
-// Add event listener for the button
 document.getElementById('load-more-btn').addEventListener('click', loadMoreProjects);
-
-// Initialize by showing the latest project
-window.onload = displayLatestProject;
