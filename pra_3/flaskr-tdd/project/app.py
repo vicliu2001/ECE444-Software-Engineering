21aabf0980dd1aa22269f632ea3d9ae24bdae4ae
vicliu2001 <116909592+vicliu2001@users.noreply.github.com>
2024-10-01 22:30:56 -0400

    added port specification

--- pra_3/flaskr-tdd/project/app.py
+++ pra_3/flaskr-tdd/project/app.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import os
 from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort, jsonify
 
 # configuration
@@ -112,4 +112,6 @@ def delete_entry(post_id):
 
 
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+    port = int(os.getenv("PORT", 5000))  # Fallback to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
